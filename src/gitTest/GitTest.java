565c40f1806f46fc08d900f438579107a384dd86
Taositong <3599720784@qq.com>
2024-05-12 16:01:36 +0800

    first commit

---
+++ src/gitTest/GitTest.java
@@ -0,0 +1,16 @@
+package gitTest;
+
+/**
+ * ClassName: GitTest
+ * Package: gitTest
+ * Description:
+ *
+ * @Author 陶思彤
+ * @Create 12/5/2024 下午3:56
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
